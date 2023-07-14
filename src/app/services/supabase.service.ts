a26bcf843a59c62ee69d137c5911ddf7f180bd55
sunguti-albright <sungutialbright@gmail.com>
2023-07-14 15:50:50 +0300

    add supabase key, url and service

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import {environment} from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupabaseService {
-
-  constructor() { }
+public client : SupabaseClient;
+  constructor() { 
+    this.client = createClient(environment.superbaseUrl,  environment.superbaseKey)
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+production : false,
+superbaseUrl : 'https://mksgucipbyrqeuitawlr.supabase.co',
+superbaseKey : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rc2d1Y2lwYnlycWV1aXRhd2xyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk0MjMyODcsImV4cCI6MjAwNDk5OTI4N30.FAN6g43fKRyBf8K0jqNTwsS_4WDlxFFMDlVKMRsgbeA'
+
+}
