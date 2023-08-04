352fade257a4fa6f7f5be7544ba1c1640d15a869
sunguti-albright <sungutialbright@gmail.com>
2023-08-04 12:19:43 +0300

    chore: get current user

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from '@angular/core';
-import { SupabaseClient, createClient } from '@supabase/supabase-js';
-import {environment} from 'src/environments/environment'
+import { SupabaseClient, User, createClient } from '@supabase/supabase-js';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupabaseService {
-public client : SupabaseClient;
-  constructor() { 
+  private client: SupabaseClient;
+  private _currentUser : BehaviorSubject<boolean | User | any > = new BehaviorSubject(null);
+  
+  constructor() {
+    this.client = createClient(
+      environment.superbaseUrl,
+      environment.superbaseKey
+    );
+
+    const user = this.client.auth.getUser();
+    console.log('user :' , user);
+    
+if(){
+
+}
+
+    this.client.auth.onAuthStateChange((event, session) => {
+      console.log('event', event);
+      console.log('session', session);
+
+      if (event === 'SIGNED_IN') {
+        console.log('signed in event');
+      } else {
+      }
+    });
+  }
 
 //supabase email sign in
   signInWithEmail(email : string){
