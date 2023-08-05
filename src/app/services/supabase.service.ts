4f6c2d6c63635bdc5fb6217849c21af06e420db1
sunguti-albright <sungutialbright@gmail.com>
2023-08-05 12:43:43 +0300

    get current user as observable

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { SupabaseClient, User, createClient } from '@supabase/supabase-js';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -8,20 +9,23 @@ import { environment } from 'src/environments/environment';
 })
 export class SupabaseService {
   private client: SupabaseClient;
-  private _currentUser : BehaviorSubject<boolean | User | any > = new BehaviorSubject(null);
-  
-  constructor() {
+  private _currentUser: BehaviorSubject<boolean | User | any> =
+    new BehaviorSubject(null);
+
+  constructor(private router : Router) {
     this.client = createClient(
       environment.superbaseUrl,
       environment.superbaseKey
     );
 
-    const user = this.client.auth.getUser();
-    console.log('user :' , user);
-    
-if(){
+    const user = await this.client.auth.getUser();
+    console.log('user :', user);
 
-}
+    if (user) {
+      this._currentUser.next(user);
+    } else {
+      this._currentUser.next(false);
+    }
 
     this.client.auth.onAuthStateChange((event, session) => {
       console.log('event', event);
@@ -29,20 +33,28 @@ if(){
 
       if (event === 'SIGNED_IN') {
         console.log('signed in event');
+        this._currentUser.next(session?.user)
       } else {
+        this._currentUser.next(false);
+        this.router.navigateByUrl("/", {replaceUrl: true})
       }
     });
   }
 
-//supabase email sign in
-  signInWithEmail(email : string){
+  //supabase email sign in
+  signInWithEmail(email: string) {
     return this.client.auth.signInWithOtp({
       email,
-    })
+    });
   }
 
-//logout
-  logoutUser(){
-    this.client.auth.signOut()
+  //logout
+  logoutUser() {
+    this.client.auth.signOut();
+  }
+  
+//method to export current user from private constructor
+  get currentUser(){
+    return this._currentUser.asObservable();
   }
 }
